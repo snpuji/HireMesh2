9c47229f83b3c2607deda6206aeb56c61e4a6200
snpuji <srinuryanipujiastuti@gmail.com>
2025-10-06 17:09:19 +0700

    update

---
+++ academy.php
@@ -0,0 +1,17 @@
+
+<!-- Find Jobs Page -->
+<link rel="stylesheet" href="css/academy.css" />
+<body>
+
+    <div class="company-section">
+        <h2 class="section-title">Boost Your Skills, Land Global Opportunities</h2>
+        <h6 class="caption-title">Learn from top Indonesian companies and industry experts to prepare yourself for high-demand remote jobs worldwide.</h6>
+        <div class="card-container">
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">Gao Tek Inc.</p><p class="job-count">5 Jobs</p></div>
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">TenTwenty</p><p class="job-count">5 Jobs</p></div>
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">CleverTqp</p><p class="job-count">5 Jobs</p></div>
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">River Spring Lodge</p><p class="job-count">5 Jobs</p></div>
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">Lemon.io</p><p class="job-count">5 Jobs</p></div>
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">Lemon.io</p><p class="job-count">5 Jobs</p></div>
+            </div>
+    </div>
---
+++ companies.php
@@ -0,0 +1,14 @@
+<!-- Find Jobs Page -->
+<link rel="stylesheet" href="css/company.css" />
+
+    <div class="company-section">
+        <h2 class="section-title">All Companies (5)</h2>
+        <div class="card-container">
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">Gao Tek Inc.</p><p class="job-count">5 Jobs</p></div>
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">TenTwenty</p><p class="job-count">5 Jobs</p></div>
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">CleverTqp</p><p class="job-count">5 Jobs</p></div>
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">River Spring Lodge</p><p class="job-count">5 Jobs</p></div>
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">Lemon.io</p><p class="job-count">5 Jobs</p></div>
+            <div class="company-card"><div class="logo-placeholder"></div><p class="company-name">Lemon.io</p><p class="job-count">5 Jobs</p></div>
+            </div>
+    </div>
